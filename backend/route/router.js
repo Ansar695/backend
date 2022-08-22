3ddcb9cda44ddeaab36b1751627db1f9068f6f00
Ansar Saeed <ansar.saeed@emumba.com>
2022-08-22 15:09:24 +0500

    removed unsable code

--- backend/route/router.js
+++ backend/route/router.js
@@ -7,7 +7,6 @@ const {
     UpdateEmployee, 
     deleteEmployee,
     searchEmployes} = require("../Controllers/usersController")
-
 const { 
     signUpEmployee, 
     getUser, 
@@ -298,93 +297,4 @@ router.post("/admin_settings", async(req, res) => {
         console.error(error)
     }
 })
-
-// router.post("/add_employee", async(req, res) => {
-//     const{id, pin, duty} = req.body
-//     try {
-//         const userExist = await userModel.findOne({$and: [{id}, {pin}]})
-//         const userExist2 = await userModel.findOne({$or: [{id}, {pin}]})
-//         if(userExist){
-//             res.status(201).send(userExist)
-//         }
-//         else if(userExist2){
-//             res.status(202).send(userExist2)
-//         }
-//         else{
-//             const data = new userModel({id,pin,duty})
-//             const result = await data.save()
-//             res.status(200).send(result)
-//         }
-//     } catch (error) {
-//         console.error(error)
-//     }
-// })
-
-router.post("/delete_employee/:id", async(req, res) => {
-    const _id = req.params.id
-    try {
-        const result = await userModel.deleteOne({_id})
-        res.status(200).send(result)
-    } catch (error) {
-        console.error(error)
-    }
-})
-
-router.post("/get_user_det", async(req, res) => {
-    const _id = req.body._id
-    try {
-        const result = await userModel.findOne({_id})
-        res.status(200).send(result)
-    } catch (error) {
-        console.error(error)
-    }
-})
-
-router.post("/update_employee/:id", async(req, res) => {
-    const _id = req.params.id
-    const{id,pin,duty} = req.body
-    try {
-        const result = await userModel.updateOne(
-            {_id},
-            {$set: {id,pin,duty}}
-        )
-        res.status(200).send(result)
-    } catch (error) {
-        console.error(error)
-    }
-})
-
-router.post("/sort_users", async(req, res) => {
-    const sortD = req.body.sortD
-    try {
-        let result;
-        if(sortD=="name"){
-            result = await userModel.find().sort({name: 1})
-        }
-        else if(sortD == "email"){
-            result = await userModel.find().sort({email: 1})
-        }else{
-            result = await userModel.find().sort({id: 1})
-        }
-        res.status(200).send(result)
-    } catch (error) {
-        console.error(error)
-    }
-})
-
-router.post("/update_admin_info/:id", async(req, res) => {
-    const _id = req.params.id
-    const{name, mobile, email} = req.body
-    try {
-        const result = await adminModel.updateOne(
-            {_id},
-            {$set: {name, mobile, email}}
-        )
-        res.status(200).send(result)
-    } catch (error) {
-        console.error(error)
-    }
-})
-
-
 module.exports = router
